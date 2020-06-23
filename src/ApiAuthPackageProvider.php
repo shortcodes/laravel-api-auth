5e046df7a0fe3ba334d047a9cb808ca27e171170
Roman Szymański <r.szymanski@mindz.pl>
2020-06-23 11:28:40 +0200

    fix

--- src/ApiAuthPackageProvider.php
+++ src/ApiAuthPackageProvider.php
@@ -10,13 +10,13 @@ class ApiAuthPackageProvider extends ServiceProvider
     public function boot()
     {
         Route::macro('apiAuthRoutes', function () {
-            Route::post(config('api-auth.login.route'), config('api-auth.login.action'));
-            Route::post(config('api-auth.logout.route'), config('api-auth.logout.action'))->name(config('api-auth.logout.name'));
-            Route::post(config('api-auth.register.route'), config('api-auth.register.action'));
-            Route::post(config('api-auth.remind-password.route'), config('api-auth.remind-password.action'))->name(config('api-auth.remind-password.name'));
-            Route::post(config('api-auth.reset-password.route'), config('api-auth.reset-password.action'))->name(config('api-auth.reset-password.name'));
-            Route::get(config('api-auth.verification.route'), config('api-auth.verification.action'))->name(config('api-auth.verification.name'));
-            Route::post(config('api-auth.resend-activation.route'), config('api-auth.resend-activation.action'))->name(config('api-auth.resend-activation.name'));
+            Route::post(config('api-auth.login.route', 'login'), config('api-auth.login.action', 'Auth\LoginController@login'));
+            Route::post(config('api-auth.logout.route', 'logout'), config('api-auth.logout.action', 'Auth\LoginController@logout'))->name(config('api-auth.logout.name', 'logout'));
+            Route::post(config('api-auth.register.route'), config('api-auth.register.action', 'Auth\RegisterController@register'));
+            Route::post(config('api-auth.remind-password.route', 'register'), config('api-auth.remind-password.action', 'Auth\ForgotPasswordController@sendResetLinkEmail'))->name(config('api-auth.remind-password.name', 'password.email'));
+            Route::post(config('api-auth.reset-password.route', 'password/email'), config('api-auth.reset-password.action', 'Auth\ResetPasswordController@reset'))->name(config('api-auth.reset-password.name', 'password.update'));
+            Route::get(config('api-auth.verification.route', 'password/reset'), config('api-auth.verification.action', 'Auth\VerificationController@verify'))->name(config('api-auth.verification.name', 'verification.verify'));
+            Route::post(config('api-auth.resend-activation.route', 'email/verify/{id}/{hash}'), config('api-auth.resend-activation.action', 'Auth\VerificationController@resend'))->name(config('api-auth.resend-activation.name', 'verification.resend'));
         });
 
         $this->publishes([
